0d4b3354f616bf657a2f2874b8b3200a68031a71
Sergio Infante <raulsergio9@gmail.com>
2020-07-02 08:13:58 -0500

    Add document upload endpoint

--- PDFParser/urls.py
+++ PDFParser/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from documents.views import document_upload
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('pdf/upload/', document_upload, name='document_upload')
 ]
--- documents/views.py
+++ documents/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+import tabula
+from io import BytesIO
+from rest_framework import status
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import AllowAny
+from rest_framework.response import Response
 
-# Create your views here.
+
+@api_view(['POST', ])
+@permission_classes((AllowAny, ))
+def document_upload(request):
+    file_raw = request.FILES.get('file').read()
+    if file_raw:
+        pdf = tabula.read_pdf(
+            BytesIO(file_raw),
+            pages="all",
+            multiple_tables=True,
+            output_format="JSON")
+        return Response(pdf, status=status.HTTP_200_OK)
+    return Response(status=status.HTTP_400_BAD_REQUEST)
